bcd5659108c13db81d4a39b73ee854e73aea11c9
Hosam <hoza2100@student.miun.se>
2025-01-16 17:12:29 +0100

    Initial commit

---
+++ Memory-Manager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Memory-Manager
+//
+//  Created by hosan on 2025-01-16.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
